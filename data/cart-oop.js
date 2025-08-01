5be0c17d3a2ee636ccb2840e07c9fb216e6ce7b1
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-08-01 22:08:15 +0800

    Create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,99 @@
+
+function Cart(cartName) {
+  const cart = {
+    cartItems: undefined,
+
+    loadFormStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(cartName));
+      if (!this.cartItems) {
+        this.cartItems = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOpionId: '1'
+        },{
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOpionId: '2'
+        }]
+      }
+    },
+
+    saveToLocalSrorage() {
+      localStorage.setItem(cartName, JSON.stringify(this.cartItems));
+    },
+
+    addToCart(productId, selectorQuantity) {
+      let matchingItem;
+      
+      this.cartItems.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+          matchingItem = cartItem;
+          cartItem.quantity += selectorQuantity;
+        }
+      });
+
+      if (!matchingItem) {
+        this.cartItems.push({
+          productId,
+          quantity: selectorQuantity,
+          deliveryOpionId: '0'
+        });
+      }
+        
+      this.saveToLocalSrorage();
+    },
+    
+    updateDeliveryOption(productId, deliveryOpion) {
+      this.cartItems.forEach(cartItem => {
+        if (productId === cartItem.productId) {
+          cartItem.deliveryOpionId = deliveryOpion;
+          this.saveToLocalSrorage();
+        };
+      });
+    },
+
+    removeFromCart(productId) {
+      const newCart = [];
+      this.cartItems.forEach(cartItem => {
+        !(cartItem.productId === productId) && newCart.push(cartItem);
+      });  
+
+      this.cartItems = newCart;
+      this.saveToLocalSrorage();
+    },
+
+    calculateCartQuantity() {
+      let cartQuantity = 0;
+
+      this.cartItems.forEach((item) => {
+        cartQuantity += item.quantity
+      });
+
+      return cartQuantity;
+    },
+
+    updateCartQuantity(productId, newQuantity) {
+      let matchingItem; 
+
+      this.cartItems.forEach(cartItem => {
+        if (productId === cartItem.productId) { 
+          matchingItem = cartItem;
+        }
+      });
+
+      matchingItem.quantity = newQuantity;
+      this.saveToLocalSrorage();
+    }
+  };
+
+  cart.loadFormStorage()
+  return cart
+}
+
+const cartOop = Cart('cart-oop');
+
+const cartko = Cart('Cartko');
+
+
+console.log(cartOop);
+console.log(cartko);

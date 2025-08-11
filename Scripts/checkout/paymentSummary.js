fc676ebd2b27b659dcd63dc00223f74c55c17349
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-08-11 20:30:51 +0800

    Create orders using backend

--- Scripts/checkout/paymentSummary.js
+++ Scripts/checkout/paymentSummary.js
@@ -1,45 +1,50 @@
-import {
-  cart,
-  calculateCartQuantity} from "../../data/cart.js";
-import {centToDollar} from "../utils/money.js";
-import {getMatchingItem} from "../../data/products.js";
-import deliveryOptions from "../../data/deliveryOptions.js"
+import { cart, calculateCartQuantity } from "../../data/cart.js";
+import { centToDollar } from "../utils/money.js";
+import { getMatchingItem } from "../../data/products.js";
+import { addOrder, orders } from "../../data/orders.js";
+import deliveryOptions from "../../data/deliveryOptions.js";
 
 export function renderPaymentSummary() {
   let cartItemPriceInCents = 0;
   let ShippingFeePriceInCents = 0;
 
-  cart.forEach(cartItem => {
-    const matchingItem = getMatchingItem(cartItem.productId)
-    
-    const itemPriceCents = matchingItem.priceCents * cartItem.quantity;
-    cartItemPriceInCents += itemPriceCents
+  cart.forEach((cartItem) => {
+    const matchingItem = getMatchingItem(cartItem.productId);
 
-    const {deliveryOpionId} = cartItem;
-    const deliveryFeeInCents = deliveryOptions[Number(deliveryOpionId)].priceCents
-    
+    const itemPriceCents = matchingItem.priceCents * cartItem.quantity;
+    cartItemPriceInCents += itemPriceCents;
+
+    const { deliveryOpionId } = cartItem;
+    const deliveryFeeInCents =
+      deliveryOptions[Number(deliveryOpionId)].priceCents;
+
     ShippingFeePriceInCents += deliveryFeeInCents;
   });
 
   const beforeTax = cartItemPriceInCents + ShippingFeePriceInCents;
-  const estimatedTax = beforeTax * 0.10;
-  
+  const estimatedTax = beforeTax * 0.1;
+
   const totalCost = beforeTax - estimatedTax;
-  
-  const paymentSummary = 
-  `
+
+  const paymentSummary = `
   <div class="payment-summary-title">
     Order Summary
   </div>
 
   <div class="payment-summary-row">
-    <div>Item${(calculateCartQuantity() <= 1) ? '' :'s'} (${calculateCartQuantity()}):</div>
-    <div class="payment-summary-money">$${centToDollar(cartItemPriceInCents)}</div>
+    <div>Item${
+      calculateCartQuantity() <= 1 ? "" : "s"
+    } (${calculateCartQuantity()}):</div>
+    <div class="payment-summary-money">$${centToDollar(
+      cartItemPriceInCents
+    )}</div>
   </div>
 
   <div class="payment-summary-row">
     <div>Shipping &amp; handling:</div>
-    <div class="payment-summary-money">$${centToDollar(ShippingFeePriceInCents)}</div>
+    <div class="payment-summary-money">$${centToDollar(
+      ShippingFeePriceInCents
+    )}</div>
   </div>
 
   <div class="payment-summary-row subtotal-row">
@@ -60,8 +65,31 @@ export function renderPaymentSummary() {
   <button class="place-order-button button-primary">
     Place your order
   </button>
-  `
-  document.querySelector('.payment-summary').innerHTML = paymentSummary;
+  `;
+
+  document.querySelector(".payment-summary").innerHTML = paymentSummary;
+
+  document
+    .querySelector(".place-order-button")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+
+        const order = await response.json();
+
+        addOrder(order);
+      } catch (error) {
+        alert(error)
+      }
+    });
 }
 
 export default renderPaymentSummary;
